2dd353d9b0a78ce22e17e422f597596cc7fedab5
elhusseinn <elhusseinabdalaziz1@gmail.com>
2022-06-04 03:29:31 +0200

    edited minimax algorithm Co-authored-by: dovaaa <dovaaa@users.noreply.github.com>

--- PC_Move_Controller/controller.py
+++ PC_Move_Controller/controller.py
@@ -12,6 +12,20 @@ def get_board_positions(board, x):
     return currentPosition
 
 # function for hueristic calculation (takes a state and return a value)
+def get_deepest_empty_goal(board, player):
+    # deepestLevel = None
+    # deepestIndex = None
+    # if player == 1: # goal starts at [16][12]
+    #     for level in range(0, 4):
+    #         for index in range(9, 16):
+    #             # if board[level][index] = 0 :
+                    
+                
+    # elif player == 2: # goal starts at [0][12]
+    #     return 0
+    return 1
+
+
 
 def calculate_heuristic(board):
     Ai_positions = get_board_positions(board, 2)
@@ -29,7 +43,7 @@ def generateStates(board,x): # takes a state and a player type(1 for player, 2 f
     tempBoard = board.copy()
     states = [] 
     positions = get_board_positions(board,x) # gets all the positions of all my marbles in the board
-    for position in positions:
+    for position in positions: # each marble i own in the playing ground
         level = position[0]
         index = position[1]
         availableMoves = BD.getAvailableMoves(board,level,index)
@@ -41,41 +55,70 @@ def generateStates(board,x): # takes a state and a player type(1 for player, 2 f
 
     return states
 
+def getBestState(states):
+    bestMove=[]
+    for state in states:
+            heuristic = calculate_heuristic(state)
+            if (heuristic > bestheuristic):
+                    bestheuristic = heuristic
+                    bestMove = state
+                   
+    return bestMove
 # function that applies minimax , uses heuristic and generateStates functions
 
-def miniMax(board, depth, maximizingAgent): # takes board(cuurent state) , return the best move i can make in terms of [marble-> to position]
+def miniMax (board, depth, maximizingAgent): # takes board(cuurent state) , return the best move i can make in terms of [marble-> to position]
     result = BD.checkWin(board)
-    if(depth == 0 or result != -1):
-        return calculate_heuristic(board)
+    bestMove = []
+    bestheuristic= -math.inf
+    # if(depth == 1 or result != -1):
+    #      states = generateStates(board,maximizingAgent)
+    #      return getBestState(states)
+    
+    
+
     if(maximizingAgent == 2):
-        finalScore = -math.inf
-        # recursively get the states
-        tempBoard = board.copy()
-        positions = get_board_positions(board,maximizingAgent)
-        for position in positions:
-            level = position[0]
-            index = position[1]
-            availableMoves = BD.getAvailableMoves(board,level,index)
-            for move in availableMoves:
-                tempBoard[level][index]  = 0  # removes the marble from the current position
-                tempBoard[move[0]][move[1]] = maximizingAgent   # put the marble in the next position which generates the state
-                score = miniMax(tempBoard, depth-1, 1)
-                tempBoard = board.copy()
-                finalScore = max(finalScore, score)
-        return finalScore
+        bestheuristic ,bestMove = maxValue(board,depth)
+        
     else:
-        finalScore = math.inf
-        # here we recursively get the states
-        tempBoard = board.copy()
-        positions = get_board_positions(board,maximizingAgent)
-        for position in positions:
-            level = position[0]
-            index = position[1]
-            availableMoves = BD.getAvailableMoves(board,level,index)
-            for move in availableMoves:
-                tempBoard[level][index]  = 0          # removes the marble from the current position
-                tempBoard[move[0]][move[1]] = maximizingAgent       # put the marble in the next position which generates the state
-                score = miniMax(tempBoard, depth-1, 2)
-                tempBoard = board.copy()
-                finalScore = min(finalScore, score)
-        return finalScore
+       bestheuristic ,bestMove = minValue(board,depth)
+       
+    return bestMove
+
+
+def maxValue(state,depth):
+    result = BD.checkWin(state)
+    if(result!=-1):
+        if(result ==1):
+              return float('-inf'), None
+        else:
+            return float('inf'), None
+    elif(depth==0):
+        return calculate_heuristic(state) ,state
+    
+    heur=-math.inf
+    maxState= None
+    for state1 in generateStates(state,2):
+        temp, _ = minValue(state,depth-1)
+        if(heur<temp):
+            heur=temp
+            maxState=state1
+    return heur,maxState
+
+def minValue(state,depth):
+    result = BD.checkWin(state)
+    if(result!=-1):
+        if(result ==1):
+              return float('-inf'), None
+        else:
+            return float('inf'), None
+    elif(depth==0):
+        return calculate_heuristic(state) ,state
+
+    heur = math.inf
+    minState=None
+    for state1 in generateStates(state,1):
+        h1, _ = maxValue(state1,depth-1)
+        if(heur>h1):
+            heur=h1
+            minState = state1
+    return heur,minState
--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from GameManager import board as BD
 from PC_Move_Controller import controller as controller
 
 board=BD.init_board()
-newBoard = board.copy()
-states = controller.generateStates(board, 2)
+# board[3][11] = 0
+# board[4][10] = 1
+# board[14][14] = 0
+# board[12][16] = 2
+# board[3][9] = 0
+# board[5][11] = 1
+# board[14][10] = 0
+# board[12][12] = 2
+# board[3][9] = 0
+# board[5][11] = 1
 
-for state in states:
-    BD.printBoard(state)
+# BD.printBoard(board)
+for i in range(20):
+    board = controller.miniMax(board, 2, 2)
+    BD.printBoard(board) 
 
 
 
